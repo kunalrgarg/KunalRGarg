85343ff833122c7f24bbd0705e753e92b2ca7f1a
kunalrgarg <kunalrgarg@gmail.com>
2019-01-04 01:26:45 -0500

    some more changes

--- script.js
+++ script.js
@@ -4,4 +4,38 @@ $(document).ready(function() {
 		play: 5000,
 		pagination: false,
 	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Student", "Software Engineer", "Catan Tryhard", "Killer Whale Enthusiast"],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false,
+	});
+
+	var owl = $('.owl-carousel');
+	owl.owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	});
+	owl.on('mousewheel', '.owl-stage', function (e) {
+	    if (e.deltaX>0) {
+	        owl.trigger('next.owl');
+	    } else {
+	        owl.trigger('prev.owl');
+	    }
+	    e.preventDefault();
+	});
 })
